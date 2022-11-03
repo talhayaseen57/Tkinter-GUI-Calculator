5cc0f3317c4b8497bf101f329093390cc6f52027
talhayaseen57 <talhayaseen5725@gmail.com>
2022-11-03 21:23:38 +0100

    created display labels

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 
+SMALL_FONT_STYLE = ("Arial", 16)
+LARGE_FONT_STYLE = ("Arial", 40, "bold")
 
 LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
 
 class Calculator:
     def __init__(self):
@@ -10,10 +13,24 @@ class Calculator:
         self.window.resizable(False, False)
         self.window.title("Calculator App")
 
+        self.total_expression = "0"
+        self.current_expression = "0"
+
         self.display_frame = self.create_display_frame()
+        self.total_label, self.label = self.create_disply_labels()
+        
         self.button_frame = self.create_button_frame()
         
     
+    def create_disply_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill="both")
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill="both")
+
+        return total_label, label
+
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=215, bg=LIGHT_GRAY)
         frame.pack(expand=True, fill="both")
